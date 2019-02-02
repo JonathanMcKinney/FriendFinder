0f05c7ca8135b24aa1046d5bc536cd5f06d78870
J.McKinney <jsmckinney89@gmail.com>
2019-02-02 16:10:32 -0500

    initial js update

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friendArr = [
+    {
+        name: "Ted Bundy",
+        email: "dark@hunger.com",
+        image: "",
+        qOne: 5,
+        qTwo: 5,
+        qThree: 5,
+        qFour: 5,
+        qFive: 5,
+        qSix: 5,
+        qSeven: 5,
+        qEight: 5,
+        qNine: 5,
+        qTen: 5,
+        qEleven: 5,
+        qTwelve: 5        
+    },
+];
+
+module.exports = friendArr;   
