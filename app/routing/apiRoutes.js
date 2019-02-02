43f2c1dd3ec9262553a3c2955220a81b0da727ba
J.McKinney <jsmckinney89@gmail.com>
2019-02-02 16:43:27 -0500

    linked routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,8 +4,8 @@ module.exports = function(app){
     app.get("/api/friends", function(req, res){
         res.json(friendsList);
     });
+    
+    app.post('/api/friends', function(req, res){
+        friendsList.push(req.body);
+    })
 };
-
-app.post('/api/friends', function(req, res){
-    friendsList.push(req.body);
-})
--- server.js
+++ server.js
@@ -4,11 +4,11 @@ var app = express();
 
 var PORT = process.env.PORT || 8000;
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./app/routing/apiRoutes');
-require('./app/routing/htmlRoutes');
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
 
 app.listen(PORT, function(){
     console.log("Listening on Port: " + PORT);
